13c65bc5f4eacdf8992fc572dbc5fe2a3b2b4753
Cody Buehler <Cody.Buehler@ameritas.com>
2022-02-01 13:35:54 -0600

    Moved testthat utils to test folder to avoid importing instead of suggesting

--- R/utils.R
+++ R/utils.R
@@ -118,20 +118,3 @@ NULL
 #' @param rhs A function call using the magrittr semantics.
 #' @return The result of calling `rhs(lhs)`.
 NULL
-
-# testthat helpers -------------------------------------------------------------
-
-# Check attribute preservation after calling a method.
-attr_preserved <- function(x, f, ...) {
-  x_meta <- attributes(x)[c('metric_variables', 'metrics_applied')]
-
-  y <- do.call(f, list2(x, ...))
-  y_meta <- attributes(y)[c('metric_variables', 'metrics_applied')]
-
-  expect_equal(x_meta, y_meta)
-}
-
-# Check class preservation after calling a method.
-class_preserved <- function(x, f, ...) {
-  expect_s3_class(do.call(f, list2(x, ...)), class = 'tbl_es')
-}
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,4 @@
+# data -------------------------------------------------------------------------
 es <- expstudy(
   data = mortexp,
   actuals = ACTUAL_DEATHS,
@@ -6,3 +7,16 @@ es <- expstudy(
   variances = VARIANCE_DEATHS,
   keys = c(AS_OF_DATE, POLICY_HOLDER)
 )
+
+# utils ------------------------------------------------------------------------
+attr_preserved <- function(x, f, ...) {
+  x_meta <- attributes(x)[c('metric_variables', 'metrics_applied')]
+
+  y <- do.call(f, list2(x, ...))
+  y_meta <- attributes(y)[c('metric_variables', 'metrics_applied')]
+
+  testthat::expect_equal(x_meta, y_meta)
+}
+class_preserved <- function(x, f, ...) {
+  expect_s3_class(do.call(f, list2(x, ...)), class = 'tbl_es')
+}
